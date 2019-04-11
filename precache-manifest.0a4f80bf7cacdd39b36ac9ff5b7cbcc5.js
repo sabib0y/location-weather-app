802a82a4b86a86d7c7fb5907e9ded23e90136f22
paul idemudia <osaspofficial@gmail.com>
2019-04-11 10:22:29 +0100

    Updates

---
+++ precache-manifest.0a4f80bf7cacdd39b36ac9ff5b7cbcc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e06401655a4b0c5c33067c45775fed3d",
+    "url": "/location-weather-app/static/media/sunny.e0640165.jpg"
+  },
+  {
+    "revision": "584a02f7bd7a349df60787c1456a2759",
+    "url": "/location-weather-app/static/media/rainy.584a02f7.jpg"
+  },
+  {
+    "revision": "a05dcb191102a4f2742f",
+    "url": "/location-weather-app/static/js/runtime~main.39a5831f.js"
+  },
+  {
+    "revision": "9065e884a552a938082d",
+    "url": "/location-weather-app/static/js/main.551a3520.chunk.js"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/location-weather-app/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "9065e884a552a938082d",
+    "url": "/location-weather-app/static/css/main.621051bd.chunk.css"
+  },
+  {
+    "revision": "04fdd3a25dd2acf1e9955b0cbd79916c",
+    "url": "/location-weather-app/index.html"
+  }
+];

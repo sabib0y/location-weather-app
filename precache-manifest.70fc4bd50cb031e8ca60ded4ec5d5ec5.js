4fd709ae02916d6413f368e9ecc696252a1684d0
paul idemudia <osaspofficial@gmail.com>
2019-04-17 15:57:23 +0100

    Updates

--- precache-manifest.70fc4bd50cb031e8ca60ded4ec5d5ec5.js
+++ precache-manifest.70fc4bd50cb031e8ca60ded4ec5d5ec5.js
@@ -8,11 +8,15 @@ self.__precacheManifest = [
     "url": "/location-weather-app/static/media/rainy.584a02f7.jpg"
   },
   {
+    "revision": "0ca89a1e695ebd5df39b03eb13c1ef9e",
+    "url": "/location-weather-app/static/media/haze.0ca89a1e.jpg"
+  },
+  {
     "revision": "a05dcb191102a4f2742f",
     "url": "/location-weather-app/static/js/runtime~main.39a5831f.js"
   },
   {
-    "revision": "b59f29f3e04289d9fe84",
+    "revision": "6ebb127473f8e8a454c9",
     "url": "/location-weather-app/static/js/main.8befc27c.chunk.js"
   },
   {
@@ -20,11 +24,11 @@ self.__precacheManifest = [
     "url": "/location-weather-app/static/js/2.c272f57a.chunk.js"
   },
   {
-    "revision": "b59f29f3e04289d9fe84",
-    "url": "/location-weather-app/static/css/main.bc9ec629.chunk.css"
+    "revision": "6ebb127473f8e8a454c9",
+    "url": "/location-weather-app/static/css/main.291f2e5d.chunk.css"
   },
   {
-    "revision": "af807e2087c240d6dcf71fb36d18090f",
+    "revision": "50e5c1f322f822d9e6ec8094747c3101",
     "url": "/location-weather-app/index.html"
   }
 ];
